0b94693e11150eb0fe3d9d8368e1902a6129dc0a
Arobol <arobolgame@gmail.com>
2018-05-21 03:52:33 -0500

    cambios

--- build/web/Postlog_admin.jsp
+++ build/web/Postlog_admin.jsp
@@ -13,6 +13,39 @@
         <title>JSP Page</title>
     </head>
     <body>
-     
+
+        <br>
+        <div class="container"><br><br><br>
+            <h1>
+                Bienvenido, <%out.println(models.user.get_active_user().get_nikname());%><br>
+            </h1>
+            <h5>
+                Te has logeado como Administrador
+            </h5><br>
+            <h5>
+                ¿Qué desea hacer a continuación?
+            </h5>
+            <br><br>
+            Editar:
+            <br><br>
+            <a class="btn btn-secondary" href="#" id="titulo" role="button">Titulo</a>
+            <a class="btn btn-secondary" href="#" id="precio" role="button">Precio</a>
+            <a class="btn btn-secondary" href="#" id="descipcion" role="button">Descripción</a>
+            <button type="button" class="btn btn-secondary" data-toggle="collapse" data-target="#demo2">Eliminar</button>
+            <form action='eliminar' method='post'>
+                <div id="demo2" class="collapse"><br>
+                    <input type="text" name="name" class="form-control" placeholder="Introduzca el nombre del juego" required>
+                    <br>
+                    <button type="submit" class="btn btn-primary">Eliminar</button>
+                </div>
+            </form>
+            <br><br>
+            
+            Comentarios:
+            <br><br>
+            <a class="btn btn-secondary" href="#" id="ver_comentario" role="button">Ver comentarios</a>
+            <a class="btn btn-secondary" href="#" id="eliminar_comentario" role="button">Eliminar comentario </a>
+            
+        </div>
     </body>
 </html>
---
+++ src/java/eliminar.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+/**
+ *
+ * @author user
+ */
+public class eliminar extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String product = request.getParameter("name");
+        Iterator iter = models.product.get_products().iterator();
+        while (iter.hasNext()) {
+            models.product pro = (models.product) iter.next();
+            if (pro.get_name().equals(product)) {
+                if (models.user.get_active_user() instanceof models.seller) {
+                    response.sendRedirect("Postlog_seller.jsp");
+                } else {
+                    response.sendRedirect("Postlog_admin.jsp");
+                }
+
+            } else {
+                if (models.user.get_active_user() instanceof models.seller) {
+                    response.sendRedirect("Postlog_seller.jsp");
+                } else {
+                    response.sendRedirect("Postlog_admin.jsp");
+                }
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
